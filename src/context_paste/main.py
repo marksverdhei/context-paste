2fe52a87aab70e0af8577be9a7e5061cf9a23e2f
Markus / Mark <46672778+marksverdhei@users.noreply.github.com>
2023-07-07 21:03:10 +0200

    Add pytest and pytest ci (#2)
    * Add tests
    
    * Add pytest workflow
    
    * Fix pyproject
    
    * Test boundaries in ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/context_paste/main.py
+++ src/context_paste/main.py
@@ -1,56 +1,50 @@
 import argparse
 import os
+from typing import List
 
 import pyperclip
-from transformers import AutoTokenizer
+from transformers import GPT2Tokenizer
 
 MAX_TOKENS = 3900
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-
-templates = []
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 
-def process_files(pattern):
+def get_prompts_from_files(pattern) -> List[str]:
     if os.path.isdir(pattern):
-        for dirpath, dirnames, filenames in os.walk(pattern):
+        prompts = []
+        for dirpath, _, filenames in os.walk(pattern):
             for filename in filenames:
                 filepath = os.path.join(dirpath, filename)
-                add_file_content(filepath)
+                prompts.append(get_file_prompt(filepath))
+
+        return prompts
     elif os.path.isfile(pattern):
-        add_file_content(pattern)
+        return [get_file_prompt(pattern)]
     else:
-        print(f"Invalid pattern: {pattern}")
+        raise TypeError(f"Invalid pattern: {pattern}")
 
 
-def add_template(filename, content):
+def get_file_prompt(filename):
+    with open(filename, "r") as f:
+        content = f.read()
+
     template = """
 `{filename}`
 ```{language}
 {content}
 ```
-    """
+"""
     _, ext = os.path.splitext(filename)
     language = ext.lstrip(".")
-    templates.append(template.format(filename=filename, language=language, content=content))
-
-
-def add_file_content(filepath):
-    try:
-        with open(filepath, "r") as f:
-            content = f.read()
-    except Exception as e:
-        print(f"Error reading file {filepath}: {str(e)}")
-        return
-
-    add_template(filepath, content)
+    return template.format(filename=filename, language=language, content=content)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("pattern", help="Directory or file path pattern to process")
     args = parser.parse_args()
-    process_files(args.pattern)
-    full_string = "\n".join(templates)
+    prompts = get_prompts_from_files(args.pattern)
+    full_string = "\n".join(prompts)
     if len(tokenizer.encode(full_string)) >= MAX_TOKENS:
         print("warning, text too long. Try one file at a time")
     pyperclip.copy(full_string)
